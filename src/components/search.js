c4e892bfbaaeb360c0affbc591fb08e9d04789f9
Andrei Ispas <andreiispas838@yahoo.com>
2023-07-09 22:35:10 +0300

    stage 1 complete, at last

---
+++ src/components/search.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { AsyncPaginate } from 'react-select-async-paginate';
+import { geoApiOptions, GEO_API_URL } from '../services/api.js';
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    try {
+      return fetch(`${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions)
+        .then((response) => response.json())
+        .then((response) => {
+          return {
+            options: response.data.map((city) => {
+              return {
+                value: `${city.latitude} ${city.longitude}`,
+                label: `${city.name}, ${city.countryCode}`,
+              };
+            }),
+          };
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return <AsyncPaginate placeholder='Search for city' debounceTimeout={600} value={search} onChange={handleOnChange} loadOptions={loadOptions} />;
+};
+
+export default Search;
